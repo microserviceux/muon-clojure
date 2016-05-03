9cd91008ee1d89c1c856becd36c8df272a1edd6c
Sergio Alvarez-Napagao <tranchis@gmail.com>
2016-05-03 16:06:45 +0100

    Set version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.muoncore/muon-clojure "7.0-20160503141209"
+(defproject io.muoncore/muon-clojure "7.0-20160503141210"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Apache License, Version 2.0"

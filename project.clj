5650fc92b8330ff2428b78c56cb056f1a80f29f0
Sergio Alvarez-Napagao <tranchis@gmail.com>
2017-02-05 16:50:34 +0100

    Upgrade to version 7.1.6 of muon-core

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.muoncore/muon-clojure "7.1.5-3"
+(defproject io.muoncore/muon-clojure "7.1.6"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Apache License, Version 2.0"
@@ -21,8 +21,8 @@
                  [org.clojure/java.data "0.1.1"]
                  [midje "1.8.3"]
                  [com.stuartsierra/component "0.3.2"]
-                 [io.muoncore/muon-core "7.1.5"]
-                 [io.muoncore/muon-event "7.1.5"]
-                 [io.muoncore/muon-transport-amqp "7.1.5"]
-                 [io.muoncore/muon-discovery-amqp "7.1.5"]]
+                 [io.muoncore/muon-core "7.1.6"]
+                 [io.muoncore/muon-event "7.1.6"]
+                 [io.muoncore/muon-transport-amqp "7.1.6"]
+                 [io.muoncore/muon-discovery-amqp "7.1.6"]]
   :plugins [[lein-midje "3.2"]])

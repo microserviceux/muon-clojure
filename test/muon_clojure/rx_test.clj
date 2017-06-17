24eba5fc2af6153a442d122d1def2f4632d263c5
Sergio Alvarez-Napagao <sergio@makati.local>
2017-06-17 03:31:19 +0200

    Improve stress test

--- test/muon_clojure/rx_test.clj
+++ test/muon_clojure/rx_test.clj
@@ -1,23 +1,24 @@
 (ns muon-clojure.rx-test
   (:require [muon-clojure.rx :as rx]
-            [clojure.core.async :refer [chan to-chan <!! go-loop
+            [clojure.core.async :refer [chan to-chan <!! go-loop take
                                         sliding-buffer <! >! close!]]
             [midje.sweet :as midje]))
 
 (defn plug-subscriber [p]
   (let [res (chan 1)
-        ch (chan)
         N 16
         [s ch] (rx/subscriber N)]
     (.subscribe p s)
     (go-loop [elem (<! ch)]
+      (println s ":" elem (= elem (* N 3)))
       (if (= elem (* N 3))
-        (>! res true)
+        (do
+          (.onComplete s)
+          (close! res)
+          (println "closed"))
         (recur (<! ch))))
-    (when (<!! res)
-      (close! ch)
-      (close! res))))
+    res))
 
-(let [p (rx/publisher (fn [_] (to-chan (range 100))) nil)]
-  (dorun (map #(do (dorun (map (fn [_] (plug-subscriber p)) (range %))))
-              (range 20))))
+(let [p (rx/publisher (fn [_] (to-chan (range 100))) nil)
+      chs (mapcat #(pmap (fn [_] (plug-subscriber p)) (range %)) (range 20))]
+  (dorun (map #(<!! %) chs)))
